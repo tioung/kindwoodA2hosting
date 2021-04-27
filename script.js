1e12d1c78616263f330c131e77a90104e2c92cd7
davidkc <dakchoy@gmail.com>
2021-04-27 20:49:42 +0400

    to15doto

--- script.js
+++ script.js
@@ -72,3 +72,31 @@ play.addEventListener('click',()=>{
         start=true
     }
 })
+
+//CONTACT
+const contactForm=document.querySelector('.contact-form')
+
+contactForm.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  const name=contactForm.name.value
+  const email=contactForm.email.value
+  const subject=contactForm.subject.value
+  const telephone=contactForm.telephone.value
+  const message=contactForm.message.value
+
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "dakchoy@gmail.com",
+    Password: "txscgswevqbzxhiy",
+    To: 'dakchoy@gmail.com',
+    From: `${email}`,
+    Subject: `Kindwood: ${subject}`,
+    Body: `Name: ${name}, Email: ${email}, Subject: ${subject}, Tel: ${telephone}<br><br>
+          Message: ${message}`,
+  })
+  .then(function (message) {
+    alert("Thank you for your email. We will get back to you soon")
+  });
+
+  contactForm.reset()
+})
