ddead41286ebc86c07d519c23c1cb2c8508ca9b9
davidkc <dakchoy@gmail.com>
2022-05-19 17:18:38 +0400

    replaced gmail mailer with form submit

--- script.js
+++ script.js
@@ -74,29 +74,29 @@ play.addEventListener('click',()=>{
 })
 
 //CONTACT
-const contactForm=document.querySelector('.contact-form')
+// const contactForm=document.querySelector('.contact-form')
 
-contactForm.addEventListener("submit",(e)=>{
-  e.preventDefault()
-  const name=contactForm.name.value
-  const email=contactForm.email.value
-  const subject=contactForm.subject.value
-  const telephone=contactForm.telephone.value
-  const message=contactForm.message.value
+// contactForm.addEventListener("submit",(e)=>{
+//   e.preventDefault()
+//   const name=contactForm.name.value
+//   const email=contactForm.email.value
+//   const subject=contactForm.subject.value
+//   const telephone=contactForm.telephone.value
+//   const message=contactForm.message.value
 
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "dakchoy@gmail.com",
-    Password: "txscgswevqbzxhiy",
-    To: 'dakchoy@gmail.com',
-    From: `${email}`,
-    Subject: `Kindwood: ${subject}`,
-    Body: `Name: ${name}, Email: ${email}, Subject: ${subject}, Tel: ${telephone}<br><br>
-          Message: ${message}`,
-  })
-  .then(function (message) {
-    alert("Thank you for your email. We will get back to you soon")
-  });
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "dakchoy@gmail.com",
+//     Password: "txscgswevqbzxhiy",
+//     To: 'dakchoy@gmail.com',
+//     From: `${email}`,
+//     Subject: `Kindwood: ${subject}`,
+//     Body: `Name: ${name}, Email: ${email}, Subject: ${subject}, Tel: ${telephone}<br><br>
+//           Message: ${message}`,
+//   })
+//   .then(function (message) {
+//     alert("Thank you for your email. We will get back to you soon")
+//   });
 
-  contactForm.reset()
-})
+//   contactForm.reset()
+// })
